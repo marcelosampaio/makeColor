fcccfd1de288e97520da172230a96a664a3c361c
Marcelo Sampaio <marcelo@artlogica.com.br>
2013-12-13 00:42:46 -0200

    Initial Commit

---
+++ makeColor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  makeColor
+//
+//  Created by Marcelo Sampaio on 12/13/13.
+//  Copyright (c) 2013 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ makeColor/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  makeColor
+//
+//  Created by Marcelo Sampaio on 12/13/13.
+//  Copyright (c) 2013 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
